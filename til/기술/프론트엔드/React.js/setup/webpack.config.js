653967aa2d3a56391d8c066e70a16dc6d14ba044
yoonhona <lucky72nyh@gmail.com>
2020-03-04 16:21:46 +0900

    babel,webpack,react,typescript 기본 설정

---
+++ til/기술/프론트엔드/React.js/setup/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, '/src/index.tsx')
+  },
+
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules: [
+      // we use babel-loader to load our jsx and tsx files
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/src/index.html')
+    })
+  ]
+}
