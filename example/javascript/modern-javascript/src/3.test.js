eff2ac311ab51e322769903eca9655fd7ba27fa1
yoonhona <lucky72nyh@gmail.com>
2020-09-15 23:04:44 +0900

    모던 자바스크립트 책 학습 - 3장 함수 매개 변수

--- example/javascript/modern-javascript/src/3.test.js
+++ example/javascript/modern-javascript/src/3.test.js
@@ -21,4 +21,28 @@ describe("기본 값", () => {
     // 기본 값이 지정되어 있다고 해서 arguments로 지정되지 않음
     expect(makeRequest("test").arguments).toHaveLength(1);
   });
+
+  // 3.1.4
+  test("매개변수 기본 값 표현식", () => {
+    function getValue(value = 0) {
+      return value + 5;
+    }
+    function add(first, second = getValue(first)) {
+      return first + second;
+    }
+
+    expect(add(1)).toEqual(7);
+    expect(add(1, 1)).toEqual(2);
+  });
+
+  // 3.2.2
+  test("rest 매개 변수", () => {
+    function restArgs(...args) {
+      return args;
+    }
+
+    const first = 1,
+      second = "A";
+    expect(restArgs(first, second)).toMatchObject([first, second]);
+  });
 });
